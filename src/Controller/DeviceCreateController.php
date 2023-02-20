43b2ad17d1f4d8bb9b438c1c13ab35e933db48de
stereoflo <stereoflo@stereoflo.ru>
2023-02-20 22:31:10 +0300

    refactor

--- src/Controller/DeviceCreateController.php
+++ src/Controller/DeviceCreateController.php
@@ -2,10 +2,8 @@
 
 namespace App\Controller;
 
-use App\Domain\Entity\Device;
 use App\Domain\Service\DeviceService;
 use App\Infrastructure\Dto\DeviceCreateDto;
-use App\Infrastructure\Mapper\DeviceMapper;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -26,6 +24,6 @@ class DeviceCreateController
             $dto->getLocationId(),
             $dto->getOutlets());
 
-        return new JsonResponse($this->mapper->map($e));
+        return new JsonResponse($e);
     }
 }
